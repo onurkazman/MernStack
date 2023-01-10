bb7fac62c775c88fdac7a1451507a3716a25a258
Onur Kazman <onurkazman1@gmail.com>
2023-01-10 15:41:25 -0500

    mern project created

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({path:"./config.env"});
+const port = process.env.port || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./db/record"));
+//get driver connection
+const dbo = require("./db/conn")
+
+app.listen(port, ()) => {
+    //perform a database connection when server strts
+    dbo.connectToServer(function (err){
+        if(err) console.error(err);
+    })
+}
