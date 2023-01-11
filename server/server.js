688e6abbf5ab5c3f531e606297f4e5fdba5af56f
Onur Kazman <onurkazman1@gmail.com>
2023-01-11 11:09:04 -0500

    Connected to MongoDB

--- server/server.js
+++ server/server.js
@@ -7,11 +7,12 @@ app.use(cors());
 app.use(express.json());
 app.use(require("./db/record"));
 //get driver connection
-const dbo = require("./db/conn")
+const dbo = require("./db/conn");
 
-app.listen(port, ()) => {
+app.listen(port, () => {
     //perform a database connection when server strts
     dbo.connectToServer(function (err){
         if(err) console.error(err);
-    })
-}
+    });
+    console.log(`Server is running on port: ${port}`)
+});
